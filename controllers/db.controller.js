24b9cc1f8633744f0e658f41e8bc4a2148f59986
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-12-17 01:02:23 -0300

    Fix : "Give all data not only one Row"

--- controllers/db.controller.js
+++ controllers/db.controller.js
@@ -8,9 +8,8 @@ const get = async (req, res) => {
     await client.connect()
 
     const response = await client.query('SELECT * FROM public.clientes ORDER BY cliente_id ASC ')
-    const result = response.rows[0]
     console.log("Consulta db")
-    console.log(result) // Hello world!
+    console.log(response.rows) // Hello world!
 
     await client.end()
     res.send("db route")
