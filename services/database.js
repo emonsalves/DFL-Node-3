ee0509860e21fcd68367278cee59ebba71aaee8d
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-12-22 21:13:24 -0300

    Fix : "Fix some problems and finish"

--- services/database.js
+++ services/database.js
@@ -1,13 +1,6 @@
 const { Pool } = require('pg');
-
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'likeme',
-    password: 'sa',
-    port: 5432,
-});
-
+const {config} = require('../config/dbConfig.js')
+const pool = new Pool(config);
 const query = (text, params) => pool.query(text, params);
 
 module.exports = { query };
